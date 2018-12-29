03ce71e1bd91bc33a0c31fe2382d022abe7ccb1d
garland <garlandrileyjr@gmail.com>
2018-12-29 13:34:16 -0500

    moved script to .js file to run in node

---
+++ 01 - New Variables/var-let-const.js
@@ -0,0 +1,16 @@
+//added a .js file so i could run in terminal w/ node
+
+function setWidth() {
+    var width = 100;
+    console.log(width);
+}
+  setWidth();
+
+  let age = 28;
+  if(age > 12) {
+    let dogYears = age * 7;
+    console.log(`You are ${dogYears} dog years old!`);
+  }
+
+  let height = 200;
+  const key = 'abc123';

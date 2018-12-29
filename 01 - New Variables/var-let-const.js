06c8f9e8b1d0209cc42aefe1d28d6560fd9a84a3
garland <garlandrileyjr@gmail.com>
2018-12-29 13:52:22 -0500

    added .js to run in terminal w/ node. Played with the scope and variables as well

---
+++ 01 - New Variables/var-let-const.js
@@ -0,0 +1,18 @@
+function setWidth() {
+    var width = 100;
+    console.log(width);
+  }
+  setWidth();
+  
+
+  let age = 28;
+  if(age > 12) {
+    var dogYears = age * 7;
+    console.log(`You are ${dogYears} dog years old!`);
+    if(age > 29) {
+        console.log('Congrats! You\'re older than Bluey');
+    }
+  }
+
+  let height = 200;
+  const key = 'abc123';
